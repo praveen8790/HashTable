c068561c5f32e561e560d888a1eaa4f20c30c37f
praveen8790 <praveen8978470998rock@gmail.com>
2021-06-07 09:29:25 +0530

    CREATED ArrayList of type MyLinkedList for indexing and then storing as per the hashcode of key.

--- src/main/java/MyHashTable.java
+++ src/main/java/MyHashTable.java
@@ -1,26 +1,49 @@
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class MyHashTable<T extends Comparable<T>,V> {
 
+    ArrayList<MyLinkedList> arrayList;
 
-    MyLinkedList<T> list ;
 
     public MyHashTable() {
-        this.list = new MyLinkedList<>();
+        this.arrayList = new ArrayList<>(10);
+        for (int i = 0; i < 10; i++) {
+            this.arrayList.add(null);
+        }
+    }
+    public int getHash(T key){
+        return (Math.abs(key.hashCode()) % 10);
     }
     public  V get(T key){
-        MyMapNode<T,V> mapNode =(MyMapNode<T,V>) this.list.search(key);
-        return (mapNode == null)? null: mapNode.getValue();
+        MyLinkedList list = this.arrayList.get(getHash(key));
+        if (list == null)
+            return null;
+        MyMapNode<T,V> mapNode =(MyMapNode<T,V>) list.search(key);
+        return (mapNode == null)? null : mapNode.getValue();
     }
     public void add(T key, V value){
-        MyMapNode<T,V> mapNode = (MyMapNode<T,V>) this.list.search(key);
-        if (mapNode == null){
-            mapNode = new MyMapNode<>(key,value);
-            this.list.append(mapNode);
+        int index =getHash(key);
+        MyLinkedList list = this.arrayList.get(index);
+        if(list == null){
+            list = new MyLinkedList();
+            MyMapNode node = new MyMapNode(key,value);
+            list.append(node);
+
         }
         else
-            mapNode.setValue(value);
+        {
+            MyMapNode<T,V> mapNode = (MyMapNode<T,V>) list.search(key);
+            if (mapNode == null){
+                mapNode = new MyMapNode<>(key,value);
+                list.append(mapNode);
+            }
+            else
+                mapNode.setValue(value);
+        }
+        this.arrayList.set(index,list);
     }
+
 
     public static void main(String[] args) {
         MyHashTable<String,Integer> hashTable = new MyHashTable();
